0c0ac422fe06474c2a492ae8d12ac08dfb867eae
Siva <sivansbhfe@gmail.com>
2020-06-06 14:07:59 +0530

    Test commit 2

--- src/main/java/ImageHoster/ImageHosterApplication.java
+++ src/main/java/ImageHoster/ImageHosterApplication.java
@@ -4,7 +4,6 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.ComponentScan;
-
 @SpringBootApplication
 public class ImageHosterApplication {
     public static void main(String[] args) {
